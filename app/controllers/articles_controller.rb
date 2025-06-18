2315271a7861c2aa1317c326e18826f186f9ab56
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-06-18 21:33:48 +0300

    Add edit and update route, action and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,25 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+    @article = Article.find(params[:id])
+  end
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
       flash[:notice] = "Article was successfully created."
       redirect_to @article
     else
-      render 'new'
+      render "new"
+    end
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was successfully updated."
+      redirect_to @article
+    else
+      render "edit"
     end
   end
 end
