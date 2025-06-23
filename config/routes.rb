d637a4efb4e69c9c77986babf591eecc33aa5077
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-06-23 15:40:27 +0300

    Add user signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
