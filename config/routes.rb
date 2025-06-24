1b07a1c0d3af699f2128c299114728e4e63e718e
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-06-24 15:20:06 +0300

    Add login form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :articles
   get "signup", to: "users#new"
   resources :users, except: [:new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
