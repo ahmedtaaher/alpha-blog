aa3ac6d86093f9a75114aff642ac426879181f82
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-06-25 16:18:35 +0300

    Restrict actions in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+  def require_user
+    unless logged_in?
+      flash[:error] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
