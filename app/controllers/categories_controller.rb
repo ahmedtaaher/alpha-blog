928835da5bc16657e544436ac7a40d694d232e41
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-06-29 20:57:47 +0300

    Add admin user requirement and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [ :show ]
+  before_action :require_admin, except: [ :index, :show ]
   def show
   end
   def index
@@ -24,5 +25,11 @@ class CategoriesController < ApplicationController
   end
   def set_category
     @category = Category.find(params[:id])
+  end
+  def require_admin
+    unless logged_in? && current_user.admin?
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
   end
 end
