1302124db8af12cfbc8cae0ef84b9a278d0542a6
Ahmed Taher <ahmedtaherferweez@gmail.com>
2025-06-29 14:22:28 +0300

    Create category and test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,5 +4,20 @@ class CategoriesController < ApplicationController
   def index
   end
   def new
+    @category = Category.new
+  end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category created successfully"
+      redirect_to @category
+    else
+      flash.now[:alert] = "Error creating category"
+      render :new
+    end
+  end
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
